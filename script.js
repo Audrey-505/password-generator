c7f8ad5775bc8e6740f4777ba9f94e8f8fca6107
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-09 11:23:07 -0500

    rewritten code using prompts

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
 // Assignment code here
 
-var characterLength = document.getElementById('numb');
-var includeLowercase = document.getElementById('lowercase');
-var includeUppercase = document.getElementById('uppercase');
-var includeNumbers = document.getElementById('numbers');
-var includeSpecial = document.getElementById('special');
-var pwForm = document.getElementById('form');
-
 var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var numbersArray = [0,1,2,3,4,5,6,7,8,9]
 var specialArray = ["~","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","?",",","."]
 
-generateBtn.addEventListener("submit", myFunction => {
-  myFunction.preventDefault()
-  var passwordLength = characterLength.value
-  var lowercaseYes = includeLowercase.checked
-  var uppercaseYes = includeUppercase.checked
-  var numbersYes = includeNumbers.checked
-  var specialYes = includeSpecial.checked
-  var passwordCreator = generatePassword(passwordLength, lowercaseYes, uppercaseYes, numbersYes, specialYes)
-  password.innerText = passwordCreator
-})
+var lengthPrompt = prompt('How long do you want your password to be? (Please select a number 8-128)')
+var lowercasePrompt = prompt('Include lowercase letters? Yes or no')
+var uppercasePrompt = prompt('Include uppercase letters? Yes or no')
+var numbersPrompt = prompt('Include numbers? Yes or no')
+var specialPrompt = prompt('Include special characters? Yes or no')
 
-function generatePassword(passwordLength, lowercaseYes, uppercaseYes, numbersYes, specialYes) {
- var pwHolder = lowercaseArray
- if (uppercaseYes) pwHolder = pwHolder.concat(uppercaseArray)
- if (numbersYes) pwHolder = pwHolder.concat(numbersArray)
- if (specialYes) pwHolder = pwHolder.concat(specialArray)
+// code above and if below needs to be in the same function
+//if (!lengthPrompt || isNaN(lengthPrompt)) {
+ // return;
+//}
 
- const passwordArray = []
- for (let i = 0; i < passwordLength; i++) {
-  const passwordCode = pwHolder[Math.floor(Math.random() * pwHolder.length)]
-  passwordArray.push(String.frompwHolder(passwordCode))
- }
- return passwordArray.join('')
+lengthPrompt = parseInt(lengthPrompt)
+lowercasePrompt = lowercasePrompt.toUpperCase();
+uppercasePrompt = uppercasePrompt.toUpperCase();
+numbersPrompt = numbersPrompt.toUpperCase();
+specialPrompt = specialPrompt.toUpperCase();
+
+if (lengthPrompt >= 8 && lengthPrompt <= 128){
+  var correctLength = lengthPrompt 
 }
 
-function myFunction() {
-  let x = document.getElementById("numb").value;
-  let text;
-  if (isNaN(x) || x < 8 || x > 128) {
-    text = "Input not valid, please input number 8-128";
-  } else {
-    text = "OK";
-  }
-  document.getElementById("demo").innerHTML = text;
+var userChoices = [];
+if (lowercasePrompt === 'YES') {
+  userChoices.concat(lowercaseArray)
+}
+if (uppercasePrompt === 'YES') {
+  userChoices.concat(uppercaseArray)
+}
+if (numbersPrompt === 'YES') {
+  userChoices.concat(numbersArray)
+}
+if (specialPrompt === 'YES') {
+  userChoices.concat(specialArray)
+}
 
+
+for (let i = 0; i < correctLength.length; i++) {
+  userChoices += Math.floor(Math.random() * correctLength)
+
+}
+
+function generatePassword() {
+  return 'hello';
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -61,4 +64,14 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+
+
+
+/* userChoices = []
+if (lowercaseArray) {
+  userChoices.concat(uppercaseArray)
+}
+if (uppercaseArray) {
+
+} */
