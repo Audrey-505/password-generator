59738ef67d149452662bf4f115baf10569dc13cd
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-09 21:38:13 -0500

    working code, the logic for the random generator works

--- script.js
+++ script.js
@@ -6,44 +6,6 @@ var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O
 var numbersArray = [0,1,2,3,4,5,6,7,8,9]
 var specialArray = ["~","!","@","#","$","%","&","*"]
 
-
-/*
-var lengthPrompt = prompt('How long do you want your password to be? (Please select a number 8-128)')
-var lowercasePrompt = prompt('Include lowercase letters? Yes or no')
-var uppercasePrompt = prompt('Include uppercase letters? Yes or no')
-var numbersPrompt = prompt('Include numbers? Yes or no')
-var specialPrompt = prompt('Include special characters? Yes or no')
-
-lengthPrompt = parseInt(lengthPrompt)
-lowercasePrompt = lowercasePrompt.toUpperCase();
-uppercasePrompt = uppercasePrompt.toUpperCase();
-numbersPrompt = numbersPrompt.toUpperCase();
-specialPrompt = specialPrompt.toUpperCase();
-
-if (lengthPrompt >= 8 && lengthPrompt <= 128){
-  var correctLength = lengthPrompt 
-}
-
-var userChoices = [];
-if (lowercasePrompt === 'YES') {
-  userChoices.concat(lowercaseArray)
-}
-if (uppercasePrompt === 'YES') {
-  userChoices.concat(uppercaseArray)
-}
-if (numbersPrompt === 'YES') {
-  userChoices.concat(numbersArray)
-}
-if (specialPrompt === 'YES') {
-  userChoices.concat(specialArray)
-}
-
-for (let i = 0; i < correctLength.length; i++) {
-  userChoices += Math.floor(Math.random() * correctLength)
-
-} 
-*/
-
 function generatePassword() {
 
 var lengthPrompt = prompt('How long do you want your password to be? (Please select a number 8-128)')
@@ -68,26 +30,31 @@ specialPrompt = specialPrompt.toUpperCase();
     return;
   }
   
+  
   var userChoices = [];
   if (lowercasePrompt === 'YES') {
-    userChoices.concat(lowercaseArray)
+    userChoices = userChoices.concat(lowercaseArray)
+    console.log(userChoices)
   }
   if (uppercasePrompt === 'YES') {
-    userChoices.concat(uppercaseArray)
+    userChoices = userChoices.concat(uppercaseArray)
+    console.log(userChoices)
   }
   if (numbersPrompt === 'YES') {
-    userChoices.concat(numbersArray)
+    userChoices = userChoices.concat(numbersArray)
+    console.log(userChoices)
   }
   if (specialPrompt === 'YES') {
-    userChoices.concat(specialArray)
+    userChoices = userChoices.concat(specialArray)
+    console.log(userChoices)
   }
   
-  
-  for (let i = 0; i < userChoices.length; i++) {
-    userChoices += Math.floor(Math.random() * lengthPrompt)
-  } 
-  console.log(userChoices)
-}
+ for (let i = 0; i < lengthPrompt; i++){
+  var shuffled = [...userChoices].sort(() => .5 - Math.random())
+  var password = shuffled.slice(0, lengthPrompt).join("")
+ }
+ return password
+} 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
